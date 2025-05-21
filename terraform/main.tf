9a7f77d3c33402757dd0135a976eda3547387596
Rodman <rodman1919@hotmail.com>
2025-05-21 14:51:22 +1000

    updated TF code new

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,6 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
   tags = { Name = "public-subnet" }
 }
 
